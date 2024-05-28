98fa578c4c11044f50da5d81b2cd427b4e02855a
sq1533 <618356@naver.com>
2024-05-28 10:43:26 +0900

    feat:json >>  pandas 변경

--- openMail.py
+++ openMail.py
@@ -48,13 +48,13 @@ def mailCheck():
     id_box = driver.find_element(By.XPATH,'//input[@id="user_id"]')
     login_button_1 = driver.find_element(By.XPATH,'//button[@id="loginStart"]')
     ActionChains(driver)
-    id = works_login['works']['id']
+    id = works_login.loc['works']['id']
     ActionChains(driver).send_keys_to_element(id_box, '{}'.format(id)).click(login_button_1).perform()
     time.sleep(1)
     #로그인 정보입력(비밀번호)
     password_box = driver.find_element(By.XPATH,'//input[@id="user_pwd"]')
     login_button_2 = driver.find_element(By.XPATH,'//button[@id="loginBtn"]')
-    password = works_login['works']['pw']
+    password = works_login.loc['works']['pw']
     ActionChains(driver).send_keys_to_element(password_box, '{}'.format(password)).click(login_button_2).perform()
     time.sleep(5)
     driver.refresh()
