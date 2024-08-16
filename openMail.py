b8e0888248ef3732984b4ccb668e2b1879ed60b0
sq1533 <618356@naver.com>
2024-08-16 13:49:46 +0900

    feat; 미사용 코드 삭제

--- openMail.py
+++ openMail.py
@@ -3,8 +3,6 @@ import requests
 import json
 import pandas as pd
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
